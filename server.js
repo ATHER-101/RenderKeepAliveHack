f0a49f2a4c96b02ff0eef8066d324b809c883578
Atharva Tijare <atharvatijare@Atharvas-MacBook-Air.local>
2025-05-09 17:46:03 +0530

    Render Keep Alive Hack

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set interval to ping itself every 5 minutes (300000 ms)
+setInterval(() => {
+    axios
+        .get(`http://localhost:${port}/ping`)  // Pings itself
+        .then((response) => {
+            console.log("Ping sent!");
+        })
+        .catch((error) => {
+            console.error("Error pinging itself:", error);
+        });
+}, 300000); // 5 minutes in milliseconds
+
+// Endpoint that will ping (to keep itself awake)
+app.get("/ping", (req, res) => {
+    res.sendStatus(200);
+    console.log("Ping received!")
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Service running on port ${port}`);
+});
